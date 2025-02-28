c13fe01988bd6a196792b58e8f73c0ae729720bf
SUPRØ <00youareworngtho00@gmail.com>
2025-03-01 03:09:13 +0530

    modified Card component

--- components/card.tsx
+++ components/card.tsx
@@ -2,90 +2,164 @@ import React from 'react';
 import {
 	View, 
 	Text, 
-	StyleSheet
+	StyleSheet,
+	TouchableOpacity,
+	ImageBackground,
+    ImageSourcePropType,
 } from 'react-native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import Color from './../constants/color';
 
-const Card = ()=>{
+type CardProps ={
+	title?: string;
+	info0?: string;
+	info1?: string;
+	info2?: string;
+	quantity?: string;
+	price?: number;
+	imageLink?: ImageSourcePropType;
+};
+
+const defaultProps:CardProps = {
+	title: 'Medicine 1',
+	info0: 'Dosage: 300mg',
+	info1: 'Form Factor: Tablet',
+	info2: 'Age restriction: 13+',
+	quantity: '15 tablets',
+	price: 370,
+	imageLink: require('./../assets/images/tablets.jpg'),
+};
+
+const Card = (props:CardProps)=>{
+	const Props = {...defaultProps,...props};
 	return(
-		<View style={styles.card}>
-			<View style={styles.context}>
-				<View style={styles.contextInfo}>
-					<Text style={{fontSize:16, color: "#1B370B", fontWeight: 700 }} >Medicine 1</Text>
-					<Text
-						style={{
-							fontSize: 13,
-							color: "#2D2D2D",
-						}}
-					>
-						Dosage: 300mg{"\n"}
-						Form Factor: Tablet{"\n"}
-						Age restriction: 13+
-					</Text>
-					<Text style={{fontSize: 16, color: "#1B370B", fontWeight: 700 }}>$100</Text>
-				</View>
-				<View style={styles.contextButton}>
-					<View style={styles.seeMore}>
-						<Text style={{color: Color.baseWhite, fontWeight: 700}}>SEE DETAILS</Text>
+		<View style={styles.mainCont}>
+			<ImageBackground imageStyle={styles.image} style={{flex:1}} source={Props.imageLink}>
+				<View style={styles.overlay}>
+					<View style={styles.cartCont}>
+						<TouchableOpacity style={styles.cartBtn}>
+							<MaterialIcons style={styles.cartIcon} name="add-shopping-cart"/>
+						</TouchableOpacity>
 					</View>
-					<View style={styles.addToCart}>
-						<MaterialIcons name="shopping-cart" size={24} color={Color.baseWhite} />
+					<View style={styles.context}>
+						<View style={styles.contextInfo}>
+							<Text style={styles.title}>{Props.title}</Text>
+							<Text style={styles.desc}>
+								{Props.info0}{"\n"}
+								{Props.info1}{"\n"}
+								{Props.info2}
+							</Text>
+						</View>
+						<View style={styles.priceDetailsCont}>
+							<View>
+								<Text style={styles.quantity}>{Props.quantity}</Text>
+								<Text style={styles.price}>${Props.price}</Text>
+							</View>
+							<TouchableOpacity style={styles.detailsBtn}>
+								<Text style={styles.detailsBtnText}>SEE DETAILS</Text>
+							</TouchableOpacity>
+						</View>
 					</View>
 				</View>
-			</View>
+			</ImageBackground>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-	card:{
+	mainCont: {
 		height: 250,
 		width: 200,
-		backgroundColor: "#D0D0D0",
-		resizeMode: 'contain',
 		borderRadius: 20,
+		overflow: "hidden",
+		backgroundColor: Color.baseWhite,
 		shadowColor: "#000",
 		shadowOffset: {width: 0, height: 2},
-		padding: 5,
-		display: "flex",
 	},
-	context:{
-		height: "60%",
-		backgroundColor: "#96BA8D",
-		borderRadius: 15,
-		paddingVertical: 5,
-		paddingHorizontal: 10,
-		marginTop: "auto",
+	image: {
+		resizeMode: "cover",
+		position: "absolute",
+		bottom: 100,
 	},
-	contextInfo:{
+	overlay: {
 		flex: 1,
 	},
-	contextButton:{
-		height: "25%",
-		//backgroundColor: Color.baseBlack,
-		display: "flex",
+	cartCont: {
+		position: "absolute",
+		top: 5,
+		right: 5,
+	},
+	cartBtn: {
+		height: 40,
+		width: 40,
+		borderRadius: "100%",
+		backgroundColor: Color.baseWhiteTransparent,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	cartIcon: {
+		fontSize: 25,
+		color: Color.seaweed
+	},
+	context: {
+		height: "55%",
+		flex: 1,
+		padding: 5,
+		borderRadius: 15,
+		position: "absolute",
+		inset: 5,
+		top: "auto",
+		backgroundColor: Color.olivine,
+	},
+	contextInfo: {
+		margin: 5,
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 700,
+		lineHeight: 18,
+		marginBottom: 2,
+		color: Color.seaweed,
+	},
+	desc: {
+		fontSize: 14,
+		lineHeight: 16,
+		color: Color.baseBlack,
+	},
+	priceDetailsCont: {
+		width: "100%",
+		position: "absolute",
+		inset: 5,
+		top: "auto",
+		flexDirection: "row",
 		justifyContent: "space-between",
 		alignItems: "center",
-		flexDirection: "row",
-		borderRadius: 10,
 	},
-	seeMore:{
-		height: "100%",
-		width: "65%",
-		backgroundColor: "#1B370B",
+	quantity: {
+		fontSize: 11,
+		fontWeight: 600,
+		color: Color.graniteGrey,
+	},
+	price: {
+		fontSize: 18,
+		fontWeight: 800,
+		lineHeight: 18,
+		color: Color.baseBlack,
+	},
+	contextButton: {
+	},
+	detailsBtn: {
+		height: 40,
+		width: "60%",
 		borderRadius: 20,
 		justifyContent: "center",
 		alignItems: "center",
+		backgroundColor: Color.seaweed
 	},
-	addToCart:{
-		height: "100%",
-		width: "30%",
-		backgroundColor: "#1B370B",
-		borderRadius: 20,
-		justifyContent: "center",
-		alignItems: "center",
+	detailsBtnText: {
+		fontWeight: 700, 
+		color: Color.baseWhite
 	},
-})
+});
 
 export default Card;
--- constants/color.tsx
+++ constants/color.tsx
@@ -1,11 +1,15 @@
 export default{
 	baseWhite: "#D8E0E2",
-	baseGreen: "#80CB69",
+	baseWhiteTransparent: "#D8E0E28D",
+	searchbarBorder: "#9E9E9E",
+	graniteGrey: "#4B4B4B",
 	baseBlack: "#1E1E1E",
-	profile: "#0095F6",
-	blueKoi: "#6FA0D6",
+	
+	baseGreen: "#80CB69",
 	olivine: "#8FB67B",
 	seaweed: "#19330E",
+	
+	profile: "#0095F6",
+	blueKoi: "#6FA0D6",
 	searchbarBase: "#99999988",
-	searchbarBorder: "#9E9E9E",
 }
